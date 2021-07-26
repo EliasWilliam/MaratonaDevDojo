5136d7e1341641927c7b3daa46c0d072480f555e
EliasWilliam <will.thebest@hotmail.com>
2021-07-26 11:42:04 -0300

    MaratonaJava

---
+++ EstruturaRepeticao/EstruturaFor.java
@@ -0,0 +1,9 @@
+package EstruturaRepeticao;
+
+public class EstruturaFor {
+    public static void main(String[] args){
+        for (int x = 0;  x<10; x++){
+            System.out.println("Valor: " + x);
+        }
+    }
+}
---
+++ EstruturaRepeticao/Tableless.java
@@ -0,0 +1,15 @@
+package EstruturaRepeticao;
+
+public class Tableless {
+
+    public static void main(String[] args) {
+
+        int x = 15;
+
+        while(x <= 18) {
+            System.out.println("Você não tem permissão para entrar");
+            x++;
+        }
+    }
+
+}
