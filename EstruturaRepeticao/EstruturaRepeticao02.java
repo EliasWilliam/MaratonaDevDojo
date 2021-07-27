2a702ef7b54303aee71d9503dbebcd56fc1abacb
EliasWilliam <will.thebest@hotmail.com>
2021-07-26 21:20:28 -0300

    Exercicios

---
+++ EstruturaRepeticao/EstruturaRepeticao02.java
@@ -0,0 +1,17 @@
+package EstruturaRepeticao;
+
+public class EstruturaRepeticao02 {
+    
+    public static void main(String[] args) {
+        //Imprima todos os numeros pares de 0 até 60
+        //  contar de dois  em dois ex: i+=2
+        // contar de dois em dois if(i % 2 == 0)
+
+        for(int i =0; i<=60; i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
