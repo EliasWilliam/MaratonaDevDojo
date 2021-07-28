d508d2d3998b5b308cf0ae3b4e02a71f76e6ec1b
EliasWilliam <will.thebest@hotmail.com>
2021-07-28 19:21:22 -0300

    Array

---
+++ EstruturaRepeticao/Listas/ArraysAula.java
@@ -0,0 +1,22 @@
+package EstruturaRepeticao.Listas;
+
+public class ArraysAula {
+    public static void main(String[]args) {
+        // byte, short, int, long, float e double 0
+        // char u000
+        // boolean false
+        // String null
+        
+        String [] nomes = new String[3];
+        nomes[0] = "Goku";
+        nomes[1] = "Vegeta";
+        nomes[2] = "Gohan";
+
+        for (int i = 0; i < 3; i++){
+            
+            System.out.println(nomes[i]);
+        }
+        
+    }
+
+}
