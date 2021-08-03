3ba917999941945674c11f7136cb61d1aa12edf9
EliasWilliam <will.thebest@hotmail.com>
2021-08-03 11:46:13 -0300

    Dominio

---
+++ EstruturaRepeticao/IntroducaoClasses/Test/EstudanteTest.java
@@ -0,0 +1,18 @@
+package EstruturaRepeticao.IntroducaoClasses.Test;
+
+import EstruturaRepeticao.IntroducaoClasses.Dominio.Estudante;
+
+
+public class EstudanteTest {
+    
+    public static void main(String[] args){
+        Estudante estudante = new Estudante();
+        estudante.nome = "Ted";
+        estudante.idade = 18;
+        estudante.sexo = 'm';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
