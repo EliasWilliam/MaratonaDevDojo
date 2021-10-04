548ac2c185883f765d2e337845b6d53a1e329e25
Elias William <39130878+EliasWilliam@users.noreply.github.com>
2021-10-04 15:20:40 -0300

    Desafio

---
+++ EstruturaRepeticao/classe/Comida.java
@@ -0,0 +1,12 @@
+package EstruturaRepeticao.classe;
+
+public class Comida {
+
+    String nome;
+    double peso;
+
+    Comida(String nome, double peso) {
+        this.nome = nome;
+        this.peso = peso;
+    }
+}
